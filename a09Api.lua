5492d001a26d5e7f7abae9f7d1fa72404b4788de
RetroGames <166970640+RetroGamesAlt@users.noreply.github.com>
2024-07-09 08:53:45 +0800

    Create a09Api.lua

---
+++ a09Api.lua
@@ -0,0 +1,7 @@
+local yes = true
+local no = false
+
+local a09Api = Instance.new("ScreenGui")
+a09Api.Name = "Do_, HttpsRequest("yes")game.Players.LocalPlayer."
+a09Api.ResetOnSpawn = no
+a09Api.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
